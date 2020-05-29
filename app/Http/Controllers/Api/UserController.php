20178d08f78b6a2dd937356ec682a2d878837a14
Gonchi <a20135298@pucp.edu.pe>
2020-05-28 23:03:32 -0700

    jsdon decode coordinates user delete

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -104,8 +104,7 @@ class UserController extends Controller
             $user->adress = $request->adress;
             $user->coordinates = json_encode($request->coordinates);
             $user->save();
-            
-            $user->coordinates = json_decode($request->coordinates);
+
             return response()->json(['status' => true, 
                 'message'=> 'Update Success',
                 'body'=> $user],
